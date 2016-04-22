c179d45fd812a33a6c9bb62adcde106df68896bf
chapnickc <chapnickc@gmail.com>
2016-04-21 20:55:32 -0500

    none

--- configure.py
+++ configure.py
@@ -1,40 +1,6 @@
 import subprocess
 
 
-def start_service():
-	"""
-	Starts the local bluetooth service.
-	"""
- 	# start the bluetooth service and initialize the deive
-	init_bluetooth_commands =  ["sudo /etc/init.d/bluetooth start", "sudo hciconfig hci up"]
-	for command in init_bluetooth_commands:
-	    p = subprocess.Popen(command.split(), stdout = subprocess.PIPE)
-	    p.wait()
-
-def pair(addr, trust = True)
-	"""
-	pair with the device
-	"""
-	# pair with the device	
-	command = 'bluez-simple-agent hci0 {}'.format(addr) 
-	
-	p = subprocess.Popen(command.split(), stdout = subprocess.PIPE)	
-	p.wait()
-
-	
-	# check if the device is already trusted
-	command = 'bluez-test-device trusted {}'
-	p = subprocess.Popen(command.split(), stdout = subprocess.PIPE)
-	p.wait()
-	output = p.communicate()[0].decode('utf-8')	
-	
-	# trust the device so that everytime the we boot we can connect
-	if int(output) == 0 and trust == True:
-	    command = 'bluez-test-device trusted {} yes'.format(addr)
-	    p = subprocess.Popen(command.split(), stdout = subprocess.PIPE)
-	    p.wait()
-
-
 def scan():
 	"""
  	Scans for available Bluetooth devices 
@@ -65,9 +31,6 @@ def scan():
 			    devices[dev_name] = dev	
 	    for key, value in devices.items():
 		    print ('{} : {}'.format(key, value))
-
-def main():
-	start_service()	
 
 
 
