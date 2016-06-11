19782a17e5c906ef789d71289d5adf325b9515a1
chapnickc <chapnickc@gmail.com>
2016-06-11 13:38:28 -0500

    added submodules

---
+++ LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/DependInfo.cmake
@@ -0,0 +1,130 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_ASM
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822/TOOLCHAIN_GCC_ARM/startup_NRF51822.S" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822/TOOLCHAIN_GCC_ARM/startup_NRF51822.S.obj"
+  )
+SET(CMAKE_ASM_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/assert.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/assert.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/board.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/board.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/critical.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/critical.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/error.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/error.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/gpio.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/gpio.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/lp_ticker_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/lp_ticker_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/mbed_interface.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/mbed_interface.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/pinmap_common.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/pinmap_common.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/rtc_time.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/rtc_time.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/semihost_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/semihost_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/ticker_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/ticker_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/us_ticker_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/us_ticker_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/wait_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/wait_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822/cmsis_nvic.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822/cmsis_nvic.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822/system_nrf51.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822/system_nrf51.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/analogin_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/analogin_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/gpio_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/gpio_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/gpio_irq_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/gpio_irq_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/i2c_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/i2c_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/pinmap.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/pinmap.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/port_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/port_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/pwmout_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/pwmout_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/serial_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/serial_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/sleep.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/sleep.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/spi_api.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/spi_api.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/twi_master.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/twi_master.c.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/us_ticker.c" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/us_ticker.c.obj"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/BusIn.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/BusIn.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/BusInOut.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/BusInOut.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/BusOut.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/BusOut.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/CAN.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/CAN.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/CallChain.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/CallChain.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Ethernet.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Ethernet.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FileBase.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FileBase.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FileLike.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FileLike.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FilePath.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FilePath.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FileSystemLike.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/FileSystemLike.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/I2C.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/I2C.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/I2CSlave.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/I2CSlave.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/InterruptIn.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/InterruptIn.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/InterruptManager.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/InterruptManager.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/LocalFileSystem.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/LocalFileSystem.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/RawSerial.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/RawSerial.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/SPI.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/SPI.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/SPISlave.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/SPISlave.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Serial.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Serial.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/SerialBase.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/SerialBase.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Stream.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Stream.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Ticker.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Ticker.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Timeout.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Timeout.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Timer.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/Timer.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/TimerEvent.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/TimerEvent.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/retarget.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/home/chapnick/repos/lifeBUD/LifeBud/mbed-src/hal/common/retarget.cpp.obj"
+  "/home/chapnick/repos/lifeBUD/LifeBud/src/test/main.cpp" "/home/chapnick/repos/lifeBUD/LifeBud/src/test/build/CMakeFiles/TEST_PROJECT.elf.dir/main.cpp.obj"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ARM_MATH_CM0"
+  "MBED_BUILD_TIMESTAMP=1399904910.34"
+  "MBED_USERNAME=rosterloh84"
+  "TARGET_M0"
+  "TARGET_MCU_NORDIC_16K"
+  "TARGET_MCU_NRF51822"
+  "TARGET_NORDIC"
+  "TARGET_NRF51822"
+  "TARGET_NRF51822_MKIT"
+  "TOOLCHAIN_GCC"
+  "TOOLCHAIN_GCC_ARM"
+  "__CORTEX_M0"
+  "__MBED__=1"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  ".."
+  "../../../mbed-src"
+  "../../../mbed-src/hal/api"
+  "../../../mbed-src/hal/common"
+  "../../../mbed-src/hal/hal"
+  "../../../mbed-src/hal/targets"
+  "../../../mbed-src/hal/targets/cmsis"
+  "../../../mbed-src/hal/targets/cmsis/TARGET_NORDIC"
+  "../../../mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822"
+  "../../../mbed-src/hal/targets/hal/TARGET_NORDIC"
+  "../../../mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822"
+  "../../../mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/Lib"
+  "../../../mbed-src/hal/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/TARGET_NRF51822_MKIT"
+  "../../../BLE_API"
+  "../../../BLE_API/public"
+  "../../../BLE_API/common"
+  "../../../BLE_API/services"
+  "../../../nRF51822"
+  "../../../nRF51822/btle"
+  "../../../nRF51822/btle/custom"
+  "../../../nRF51822/common"
+  "../../../nRF51822/nordic"
+  "../../../nRF51822/nordic/nrf-sdk"
+  "../../../nRF51822/nordic/nrf-sdk/app_common"
+  "../../../nRF51822/nordic/nrf-sdk/ble"
+  "../../../nRF51822/nordic/nrf-sdk/ble/ble_services"
+  "../../../nRF51822/nordic/nrf-sdk/ble/rpc"
+  "../../../nRF51822/nordic/nrf-sdk/s110"
+  "../../../nRF51822/nordic/nrf-sdk/sd_common"
+  "../../../nRF51822/nordic/nrf-sdk/bootloader_dfu"
+  "../../../mbed-src/hal/targets/cmsis/TARGET_NORDIC/TARGET_MCU_NRF51822/TOOLCHAIN_GCC_ARM"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
